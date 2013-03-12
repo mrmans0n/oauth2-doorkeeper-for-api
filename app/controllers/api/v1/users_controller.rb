c709aaf784b2ee10df005e9da2abf28c3a6757fc
Nacho L <nacho@nlopez.in>
2013-03-12 20:26:55 +0100

    Added more routes in users api, thus enabling API registering

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,29 +1,29 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      doorkeeper_for :all
+      doorkeeper_for :all, :except => :create
       respond_to :json
 
       def index
-        #respond_with User.all
+        respond_with User.all
+      end
+
+      def show
         user = User.find(doorkeeper_token.resource_owner_id)
         respond_with user
       end
 
-      def show
-
-      end
-
       def create
-
+        user = User.create(params[:user])
+        respond_with :api, :v1, user
       end
 
       def update
-
+         head :ok
       end
 
       def destroy
-
+         head :ok
       end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ DoroboWeb::Application.routes.draw do
   use_doorkeeper
   devise_for :users
 
+
+  # API definitions
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [ :new, :show, :edit, :destroy ]
     end
   end
 
